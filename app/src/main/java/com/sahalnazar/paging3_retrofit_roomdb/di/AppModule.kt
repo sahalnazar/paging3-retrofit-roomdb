ddc70a7fe86e06e8d68589317ae1a54d012f369c
sahalnazar <sahalnazar007@gmail.com>
2022-08-07 21:23:52 +0530

    Setup dependency injection

--- app/src/main/java/com/sahalnazar/paging3_retrofit_roomdb/di/AppModule.kt
+++ app/src/main/java/com/sahalnazar/paging3_retrofit_roomdb/di/AppModule.kt
@@ -1,6 +1,60 @@
 package com.sahalnazar.paging3_retrofit_roomdb.di
 
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.sahalnazar.paging3_retrofit_roomdb.BuildConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class) // SingletonComponent, because it need to stay through out the Application life cycle.
 object AppModule {
 
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(
+    ): OkHttpClient {
+        return OkHttpClient.Builder().apply {
+            if (BuildConfig.DEBUG) {
+                val logging = HttpLoggingInterceptor()
+                logging.level = HttpLoggingInterceptor.Level.BODY
+                addInterceptor(logging)
+            }
+            connectTimeout(5, TimeUnit.MINUTES)
+            readTimeout(5, TimeUnit.MINUTES)
+            writeTimeout(5, TimeUnit.MINUTES)
+            callTimeout(5, TimeUnit.MINUTES)
+        }.build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideJson() = Json {
+        ignoreUnknownKeys = true
+        isLenient = true
+    }
+
+    @OptIn(ExperimentalSerializationApi::class)
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        json: Json
+    ): Retrofit {
+        return Retrofit.Builder()
+            .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+            .baseUrl("BASE_URL")
+            .client(okHttpClient)
+            .build()
+    }
 
 }
