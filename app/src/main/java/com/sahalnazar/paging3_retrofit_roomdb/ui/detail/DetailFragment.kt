7ec7d559023301c7bbcbad87019cefb1ea0fb256
sahalnazar <sahalnazar007@gmail.com>
2022-08-08 20:21:18 +0530

    Setup data binding

--- app/src/main/java/com/sahalnazar/paging3_retrofit_roomdb/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/sahalnazar/paging3_retrofit_roomdb/ui/detail/DetailFragment.kt
@@ -1,8 +1,32 @@
 package com.sahalnazar.paging3_retrofit_roomdb.ui.detail
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.sahalnazar.paging3_retrofit_roomdb.R
+import com.sahalnazar.paging3_retrofit_roomdb.databinding.FragmentDetailBinding
+import com.sahalnazar.paging3_retrofit_roomdb.databinding.FragmentHomeBinding
+import com.sahalnazar.paging3_retrofit_roomdb.ui.home.HomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class DetailFragment : Fragment() {
+class DetailFragment : Fragment(R.layout.fragment_detail) {
+
+    private var _binding: FragmentDetailBinding? = null
+    private val viewModel by viewModels<DetailViewModel>()
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentDetailBinding.bind(view)
+        _binding = binding
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
--- app/src/main/java/com/sahalnazar/paging3_retrofit_roomdb/ui/home/HomeFragment.kt
+++ app/src/main/java/com/sahalnazar/paging3_retrofit_roomdb/ui/home/HomeFragment.kt
@@ -1,8 +1,30 @@
 package com.sahalnazar.paging3_retrofit_roomdb.ui.home
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.sahalnazar.paging3_retrofit_roomdb.R
+import com.sahalnazar.paging3_retrofit_roomdb.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val viewModel by viewModels<HomeViewModel>()
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentHomeBinding.bind(view)
+        _binding = binding
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
