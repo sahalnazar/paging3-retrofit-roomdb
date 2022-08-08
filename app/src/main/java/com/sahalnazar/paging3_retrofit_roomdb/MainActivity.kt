c948466ce07c3d41be53221a5cae65c10ee77fae
sahalnazar <sahalnazar007@gmail.com>
2022-08-08 20:08:03 +0530

    Setup component navigation

--- app/src/main/java/com/sahalnazar/paging3_retrofit_roomdb/MainActivity.kt
+++ app/src/main/java/com/sahalnazar/paging3_retrofit_roomdb/MainActivity.kt
@@ -2,6 +2,7 @@ package com.sahalnazar.paging3_retrofit_roomdb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -9,5 +10,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setUpNavController()
+    }
+
+    private fun setUpNavController() {
+        val navHostFragment: NavHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
     }
 }
